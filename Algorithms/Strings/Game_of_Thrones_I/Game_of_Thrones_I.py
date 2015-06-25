afd222d27c89eec1069ea7eb061e875d7e3f6e0e
baranmcl <baranmcl@gmail.com>
2015-06-25 14:50:22 -0400

    Created file

---
+++ Algorithms/Strings/Game_of_Thrones_I/Game_of_Thrones_I.py
@@ -0,0 +1,19 @@
+def palindromecheck(x):
+    poop = []
+    yep = []
+    oddcount = 0
+    for letters in x:
+        poop.append(letters)
+    for letter in poop:
+        if letter not in yep:
+            yep.append(letter)
+    for letter in yep:
+        if poop.count(letter) % 2 == 1:
+            oddcount = oddcount + 1
+    if oddcount <= 1:
+        return "YES"
+    else:
+        return "NO"
+
+a = str(input())
+print(palindromecheck(a))
